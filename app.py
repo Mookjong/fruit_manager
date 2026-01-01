580992ab99d1f681b94ccf4f41eb9ec1029e5a68
Guillaume <guillaume.coubard@yahoo.fr>
2026-01-01 11:15:42 +0100

    streamlit sidebar added

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from fruit_manager import *
 import sys
 
 
-
 inventory = load_inventory("./data/fruit_inventory.json")
 pricies = load_price("./data/price.json")
 try:
@@ -12,11 +11,19 @@ except FileNotFoundError:
     sys.exit("Exiting due to missing treasury file.")
 else:
     display_treasury(treasury)
-    
 
-    
+
 st.title("Fruit Manager")
 st.header("Inventory")
 st.table(inventory)
-    
-    
+
+
+st.sidebar.header("Harvest Fruit")
+harvest_fruit_name = st.sidebar.text_input("Fruit Name")
+harvest_fruit_quantity = st.sidebar.number_input("Quantity", min_value=1, step=1)
+if st.sidebar.button("Harvest"):
+    harvest_fruit(inventory, harvest_fruit_name, harvest_fruit_quantity)
+    save_inventory("./data/fruit_inventory.json", inventory)
+    st.sidebar.success(
+        f"Harvested {harvest_fruit_quantity} units of {harvest_fruit_name}."
+    )
